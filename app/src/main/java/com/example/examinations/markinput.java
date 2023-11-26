be33cbeb28f63631c43c78e0c5dad49428dfc6a3
SharmarkeAhmed12 <liban.aden21@students.dkut.ac.ke>
2023-11-26 15:17:20 +0300

    Lecturer functionalities.

--- app/src/main/java/com/example/examinations/markinput.java
+++ app/src/main/java/com/example/examinations/markinput.java
@@ -1,8 +1,7 @@
 package com.example.examinations;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -12,83 +11,70 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class markinput extends AppCompatActivity {
 
-    EditText reg,mark;
-    Button submit;
-    Spinner unit,assesment;
+    private EditText assignment1, assignment2, cat1, cat2, exam;
+    private Spinner studentSpinner, lecCourseSpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_markinput);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerUnits);
-// Create an ArrayAdapter using the string array and a default spinner layout.
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.units,
-                android.R.layout.simple_spinner_item
-        );
-// Specify the layout to use when the list of choices appears.
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner.
-        spinner.setAdapter(adapter);
+        // Initialize your EditText fields and Spinners
+        assignment1 = findViewById(R.id.assignment1);
+        assignment2 = findViewById(R.id.assignment2);
+        cat1 = findViewById(R.id.cat1);
+        cat2 = findViewById(R.id.cat2);
+        exam = findViewById(R.id.exam);
+        studentSpinner = findViewById(R.id.studentSpinner);
+        lecCourseSpinner = findViewById(R.id.lecCourseSpinner);
 
-        Spinner spinner1 = (Spinner) findViewById(R.id.spinnerAssessmentType);
-// Create an ArrayAdapter using the string array and a default spinner layout.
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(
-                this,
-                R.array.assessment_types,
-                android.R.layout.simple_spinner_item
-        );
-// Specify the layout to use when the list of choices appears.
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner.
-        spinner1.setAdapter(adapter1);
-
-        // view initialization
-        reg = findViewById(R.id.editTextReg_no);
-        mark = findViewById(R.id.editTextMarks);
-        submit =findViewById(R.id.btnSubmitScores);
-
-        submit.setOnClickListener(View ->{
-            // retrieve values from ui
-           String reg_number = reg.getText().toString().trim();
-           String marks1 = mark.getText().toString().trim();
-           int marks=Integer.parseInt(marks1);
-           String units = spinner.getSelectedItem().toString();
-           String mark_type = spinner1.getSelectedItem().toString();
-
-           //validate input values
-            if(reg_number.isEmpty()|| reg_number.length()<1){
-                reg.setError("enter all values");
-                Toast.makeText(markinput.this,"enter all values",Toast.LENGTH_SHORT).show();
-                return;
-            }
-            if(marks1.isEmpty()){
-                mark.setError("Please Enter valid values");
-            }
-            else if ( mark_type.equals("Assignment 1")& marks<0 || marks>10) {
-                mark.setError("Please Enter valid values");
-            }
-            else if ( mark_type.equals("Assignment 2")& marks<0 || marks>10) {
-                mark.setError("Please Enter valid values");
-            }
-            else if ( mark_type.equals("Cat 1")& marks<0 || marks>20) {
-                mark.setError("Please Enter valid values");
-            }
-            else if ( mark_type.equals("Cat 2")& marks<0 || marks>20) {
-                mark.setError("Please Enter valid values");
-            }
-            else if ( mark_type.equals("Exam")& marks<0 || marks>70) {
-                mark.setError("Please Enter valid values");
-            }
-            else {
-                Intent intent = new Intent(markinput.this, leclogin.class);
-                startActivity(intent);
-            }
-
-
+        Button submitButton = findViewById(R.id.submitMarks);
+        submitButton.setOnClickListener(v -> {
+            submitMarks();
         });
+    }
 
+    private void submitMarks() {
+        try {
+            // Retrieve marks from EditText fields
+            int assignment1Marks = Integer.parseInt(assignment1.getText().toString().trim());
+            int assignment2Marks = Integer.parseInt(assignment2.getText().toString().trim());
+            int cat1Marks = Integer.parseInt(cat1.getText().toString().trim());
+            int cat2Marks = Integer.parseInt(cat2.getText().toString().trim());
+            int examMarks = Integer.parseInt(exam.getText().toString().trim());
+
+            // Validate marks
+            if (assignment1Marks <= 0 || assignment1Marks > 10 ||
+                    assignment2Marks <= 0 || assignment2Marks > 20 ||
+                    cat1Marks <= 0 || cat1Marks > 20 ||
+                    cat2Marks <= 0 || cat2Marks > 20 ||
+                    examMarks <= 0 || examMarks > 70
+            ) {
+                // Display an error toast
+                Toast.makeText(this, "Invalid marks. Please check the values.", Toast.LENGTH_SHORT).show();
+            } else {
+                // Log the successful validation
+                Log.d("SubmitMarks", "Marks validation successful");
+
+                // Perform the submission logic here
+                // ...
+
+                // Optional: You can also check if the Spinners are selected
+                String selectedStudent = studentSpinner.getSelectedItem().toString();
+                String selectedCourse = lecCourseSpinner.getSelectedItem().toString();
+                if (selectedStudent.isEmpty() || selectedCourse.isEmpty()) {
+                    // Display an error toast for Spinners
+                    Toast.makeText(this, "Please select a student and course.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Log the successful selection
+                    Log.d("SubmitMarks", "Student: " + selectedStudent + ", Course: " + selectedCourse);
+                }
+            }
+        } catch (NumberFormatException e) {
+            // Handle the case where the input is not a valid number
+            Toast.makeText(this, "Invalid input. Please enter numeric values.", Toast.LENGTH_SHORT).show();
+            e.printStackTrace(); // Log the exception for further investigation
+        }
     }
 }
